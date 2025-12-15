b42826123765860b6ccd23f56140a4c362e540aa
junyaa97 <petrovaolgamich@gmail.com>
2025-12-15 16:41:37 +0300

    improved slider

--- js.js
+++ js.js
@@ -1,12 +1,31 @@
 const buttonPrev = document.getElementById("butPrev");
 const buttonNext = document.getElementById("butNext");
 const slides = document.querySelectorAll(".slide");
+const slidesContainer = document.querySelector(".slides")
 // slides = список, который содержит все элементы с классом slide
 let nowSlide = 0;
 function showSlide(index){
-    slides.forEach(slide => {
-        slide.style.display = "none";
-    });
-    slides [index].style.display = "flex";
+   const slideWidth = slidesContainer.clientWidth;  //команда
+   slidesContainer.style.transform = `translateX(-${index*slideWidth}px)`;  //шаблонная строка
 }
+
 showSlide(nowSlide)
+function nextSlide(){
+    if(nowSlide < slides.length-1){     //если текущий слайд меньше, чем длина списка(кол-во элементов) -1
+        nowSlide++;
+    }
+
+    showSlide(nowSlide);
+    // краткая форма записи
+}
+
+function prevSlide(){
+    if(nowSlide > 0){
+        nowSlide--;
+    } 
+
+    showSlide(nowSlide);
+}
+
+buttonNext.addEventListener("click", nextSlide);
+buttonPrev.addEventListener("click", prevSlide);
