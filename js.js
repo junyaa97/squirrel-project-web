51412e8245f27b7536993d1cd105d7b9be85b9cf
junyaa97 <petrovaolgamich@gmail.com>
2025-12-17 19:10:39 +0300

    slider for mobile version and scale

--- js.js
+++ js.js
@@ -1,31 +1,35 @@
-const buttonPrev = document.getElementById("butPrev");
-const buttonNext = document.getElementById("butNext");
-const slides = document.querySelectorAll(".slide");
-const slidesContainer = document.querySelector(".slides")
-// slides = список, который содержит все элементы с классом slide
-let nowSlide = 0;
-function showSlide(index){
-   const slideWidth = slidesContainer.clientWidth;  //команда
-   slidesContainer.style.transform = `translateX(-${index*slideWidth}px)`;  //шаблонная строка
-}
+function initSlider(sliderElem){
+    const slidesContainer = sliderElem.querySelector(".slides");
+    const slides = slidesContainer.querySelectorAll(".slide");  // slides = список, который содержит все элементы с классом slide
 
-showSlide(nowSlide)
-function nextSlide(){
-    if(nowSlide < slides.length-1){     //если текущий слайд меньше, чем длина списка(кол-во элементов) -1
-        nowSlide++;
+    let nowSlide = 0;
+    const swipe = 50;  //how much px to scroll
+    let startX = 0;
+ 
+    function showSlide(index){
+        const slideWidth = slidesContainer.clientWidth;  //команда
+        slidesContainer.style.transform = `translateX(-${index*slideWidth}px)`;  //шаблонная строка
     }
 
-    showSlide(nowSlide);
-    // краткая форма записи
+    function touchStart(touch){
+        startX = touch.touches[0].pageX;
+    }
+    function touchEnd(touch){
+        const endX = touch.changedTouches[0].pageX;  //координаты по x касание
+        const diff = startX - endX;
+        if(Math.abs(diff) < swipe) return;  //return = выход из func
+
+        if(diff > 0 && nowSlide < slides.length-1){
+            nowSlide++;
+        }
+        else if(diff < 0 && nowSlide > 0){  //иначе -
+            nowSlide--;
+        }
+        showSlide (nowSlide);
+    }
+    slidesContainer.addEventListener("touchstart", touchStart, {passive:true});
+    slidesContainer.addEventListener("touchend", touchEnd, {passive:true});  //события активации
+    
+    showSlide (nowSlide);
 }
-
-function prevSlide(){
-    if(nowSlide > 0){
-        nowSlide--;
-    } 
-
-    showSlide(nowSlide);
-}
-
-buttonNext.addEventListener("click", nextSlide);
-buttonPrev.addEventListener("click", prevSlide);
+document.querySelectorAll(".slider").forEach(initSlider);
